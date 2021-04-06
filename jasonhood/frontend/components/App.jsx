b67f15fcfd9ba187e77ae4e628957a3983424c1b
jason <jmasters1000@yahoo.com>
2021-04-05 18:50:12 -0700

    Work on signup component

--- jasonhood/frontend/components/App.jsx
+++ jasonhood/frontend/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import SignupContainer from './session/signup_container'
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <h1>Jasonhood!!!</h1>
+    {/* <h1>Jasonhood!!!</h1> */}
+    <Route path="/signup" component={SignupContainer}/>
   </div>
 );
 
--- jasonhood/frontend/components/session/signup.jsx
+++ jasonhood/frontend/components/session/signup.jsx
@@ -4,12 +4,14 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.state = {user: {
       email: "",
       first_name: "",
-      laste_name: "",
+      last_name: "",
       password: ""
-    };
+    }};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInput(type) {
@@ -19,13 +21,26 @@ class Signup extends React.Component {
   }
 
   handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
+    // this.props.signup(this.state).then(() => this.props.history.push('/'));
+    this.props.signup(this.state)
   }
 
   render() {
     return (
-      <div>
+      <div className='session-form'>
+        <span className="signup-title">Make Your Money Move</span>
+        <span className="signup-subtitle">Robinhood lets you invest in companies you love, commission-free.</span>
+        <br/>
+        <p className="signup-enter">Please enter your full legal name. Your legal name should match any form of government ID.</p>
+        <form>
+          <input type="text" value={this.state.first_name} onChange={this.handleInput('first_name')}/>
+          <input type="text" value={this.state.last_name} onChange={this.handleInput('last_name')}/>
+          <input type="text" value={this.state.email} onChange={this.handleInput('email')}/>
+          <input type="password" value={this.state.password} onChange={this.handleInput('password')}/>
 
+          <button onClick={this.handleSubmit}>Continue</button>
+          </form>
       </div>
     );
   }
