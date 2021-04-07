1c87fd06ccb8ea3787a62a505dc6d519544f29b0
jason <jmasters1000@yahoo.com>
2021-04-06 19:41:25 -0700

    Work on splash page css

--- jasonhood/frontend/components/home_page/home_page.jsx
+++ jasonhood/frontend/components/home_page/home_page.jsx
@@ -41,14 +41,27 @@ class HomePage extends React.Component {
               <div className="home-section1-3">
                 <div className="home-section-1-4">
                   <div className="home-section-1-5">
-                    <div className="home-section-1-text">
-                      <h1>Investing for</h1>
-                      <h1>Everyone</h1>
-                      <h2>Commission-free investing, plus the tools</h2>
-                      <h2>you need to put your money in motion. Sign</h2>
-                      <h2>up and get your first stock for free. Certain</h2>
-                      <h2>limitations apply.</h2>
+
+                    <div className="home-section-1-text-1">
+                      <h1>Investing for Everyone</h1>
                     </div>
+
+                    <div className="home-section-1-text-2">
+                      <span>Commission-free investing, plus the tools
+                      you need to put your money in motion. Sign
+                      up and get your first stock for free. Certain
+                      limitations apply.
+                      </span>
+                    </div>
+
+                    <div className="home-section-1-bt-1">
+                      <div className="home-section-1-bt-2">
+                        <div className="home-section-1-bt-3">
+                          <Link className="home-section-1-bt-4" to="/signup"><span>Sign Up</span></Link>
+                        </div>
+                      </div>
+                    </div>
+
                   </div>
                 </div>
               </div>
