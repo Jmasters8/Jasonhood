878556c07b35b7172c3210c41105858dfc508549
jason <jmasters1000@yahoo.com>
2021-04-07 14:21:23 -0700

    Create jbuilder partial and bootstrap current user to window

--- jasonhood/app/views/api/users/show.json.jbuilder
+++ jasonhood/app/views/api/users/show.json.jbuilder
@@ -1,3 +1,1 @@
-# json.set! @user.id do
-json.extract! @user, :id, :first_name, :last_name, :email, :buying_power
-# end
+json.partial! "api/users/user", user: @user
--- jasonhood/frontend/jasonhood.jsx
+++ jasonhood/frontend/jasonhood.jsx
@@ -6,20 +6,23 @@ import Root from './components/root';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
   // window.getState = store.getState;
   // window.dispatch = store.dispatch;
-
-  let preloadedState = undefined;
+  let store;
   if (window.currentUser) {
-    preloadedState = {
-      session: {
-        currentUser: window.currentUser
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
       }
-    };
+    }
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
   }
-  const store = configureStore(preloadedState);
-
+ 
+  const root = document.getElementById("root");
   ReactDOM.render(<Root store={store}/>, root);
 })
 
