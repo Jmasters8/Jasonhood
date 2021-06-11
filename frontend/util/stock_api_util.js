d08e6c15ad5eafec689ec714c0c7c2fb1b1250c3
jason <jmasters1000@yahoo.com>
2021-06-10 20:54:13 -0700

    Remove api key from utils

--- frontend/util/stock_api_util.js
+++ frontend/util/stock_api_util.js
@@ -2,7 +2,7 @@
 export const fetchStock = (symbol) => {
   return $.ajax({
     method: 'GET',
-    url: `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=c1o93ii37fkqrr9scqp0`
+    url: `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${window.finnhubAPIKey}`
   })
 }
 
@@ -18,22 +18,22 @@ export const fetchStockInfo = (symbol) => {
 export const fetchStockData = (symbol, start, end) => {
   return $.ajax({
     method: 'GET',
-    url: `https://finnhub.io/api/v1/stock/candle?symbol=${symbol.toUpperCase()}&resolution=5&from=${start}&to=${end}&token=c1o93ii37fkqrr9scqp0`
-    // url: `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=c1o93ii37fkqrr9scqp0`
+    url: `https://finnhub.io/api/v1/stock/candle?symbol=${symbol.toUpperCase()}&resolution=5&from=${start}&to=${end}&token=${window.finnhubAPIKey}`
+    
   })
 }
 
 export const fetchStockNews = (symbol, start, end) => {
   return $.ajax({
     method: 'GET',
-    url: `https://finnhub.io/api/v1/company-news?symbol=${symbol.toUpperCase()}&from=${start}&to=${end}&token=c1o93ii37fkqrr9scqp0`
+    url: `https://finnhub.io/api/v1/company-news?symbol=${symbol.toUpperCase()}&from=${start}&to=${end}&token=${window.finnhubAPIKey}`
   })
 }
 
 export const fetchMarketNews = () => {
   return $.ajax({
     method: 'GET',
-    url: 'https://finnhub.io/api/v1/news?category=general&token=c1o93ii37fkqrr9scqp0'
+    url: `https://finnhub.io/api/v1/news?category=general&token=${window.finnhubAPIKey}`
   })
 }
 
