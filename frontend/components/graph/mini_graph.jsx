f085a52040f8a14d31d48d761c7150cbff21d18a
jason <jmasters1000@yahoo.com>
2021-07-29 22:50:56 -0700

    Fix reference line not showing up on mini graphs

--- frontend/components/graph/mini_graph.jsx
+++ frontend/components/graph/mini_graph.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ResponsiveContainer, AreaChart, XAxis, YAxis, Area, ReferenceLine } from "recharts";
+import { ResponsiveContainer, AreaChart, XAxis, YAxis, Area, Tooltip, ReferenceLine } from "recharts";
 
 class MiniGraph extends React.Component {
   constructor(props) {
@@ -33,12 +33,13 @@ class MiniGraph extends React.Component {
             <Area className="potato" dataKey="price" stroke={`${lineColor()}`} strokeWidth={1} fill="#1e2124"/>
             <XAxis dataKey="date" hide/>
             <YAxis dataKey="price" type="number" domain={data[0], data[data.length - 1]} hide/>
-            <ReferenceLine y={data[0].price} strokeWidth={1.5} strokeHeight={1.5} strokeDasharray="1 6" stroke="lightslategray" />
+            <ReferenceLine y={data[0]} ifOverflow="extendDomain" strokeWidth={1.5} strokeHeight={1.5} strokeDasharray="1 3" stroke="lightslategray" />
           </AreaChart>
         </ResponsiveContainer>
       </div>
     )
   }
 }
+
 
 export default MiniGraph;
